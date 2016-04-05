909d9242ec2e3ad96d4a0bb4f7bcefb9e03783a0
Konstantin Macarenco <kmacarenco@tripwire.com>
2016-04-05 09:56:06 -0700

    Finished exercise 1

--- lab2/condvar-pthd.c
+++ lab2/condvar-pthd.c
@@ -14,15 +14,17 @@ pthread_cond_t cond;
 void sender() {
   printf("Sender starts ...\n");
   sleep(1);
-
-  // ... add code ...
+  pthread_mutex_lock(&lock);
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&lock);
 
 }
 
 void receiver() {
   printf("Receiver starts ...\n");
-
-  // ... add code ...
+  pthread_mutex_lock(&lock);
+  pthread_cond_wait(&cond, &lock);
+  pthread_mutex_unlock(&lock);
 
   printf("Signal received!\n");
 }
@@ -35,4 +37,5 @@ int main(int argc, char **argv) {
   pthread_create(&tid1, NULL, (void *)sender, NULL);
   pthread_create(&tid2, NULL, (void *)receiver, NULL);
   pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
 }
