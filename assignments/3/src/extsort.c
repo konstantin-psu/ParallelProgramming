0304e9ebfed8b73e7ff01b56e5a35c264ed2b25b
Konstantin Macarenco <konstan2@pdx.edu>
2016-05-11 16:57:54 -0700

    Finished file reader for hw3

--- assignments/3/src/extsort.c
+++ assignments/3/src/extsort.c
@@ -1,120 +1,152 @@
 //------------------------------------------------------------------------- 
 // This is supporting software for CS415/515 Parallel Programming.
-// Copyright (c) Portland State University.
+// Copyright (c) Portland State University
 //------------------------------------------------------------------------- 
 
-// A sequential quicksort program.
+// A simple demo program of MPI file input.
 //
-// Usage: ./qsort <N>
-// 
+// Usage: 
+//   linux> mpirun -n <#procs> extsort <dataInFile>
 //
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+#include <mpi.h>
+#include <stdlib.h>
 
-#define MINSIZE   10 		// threshold for switching to bubblesort
+#define MINSIZE   10        // threshold for switching to bubblesort
+#define DEBUG 1
 
-// Swap two array elements 
+
+struct Bucket {
+    int size;
+    int *elements;
+};
+
+// Swap two array elements
 //
 void swap(int *array, int i, int j) {
-  if (i == j) return;
-  int tmp = array[i];
-  array[i] = array[j];
-  array[j] = tmp;
-}
-
-// Initialize array.
-// - first generate [1,2,...,N]
-// - then perform a random permutation
-//
-int *init_array(int N)  {
-  int *array = (int *) malloc(sizeof(int) * N);
-  for (int i = 0; i < N; i++) {
-    array[i] = i + 1;
-  }
-  srand(time(NULL));
-  for (int i = 0; i < N; i++) {
-    int j = (rand()*1./RAND_MAX) * (N-1);
-    swap(array, i, j);
-  }
-  printf("Initialized array to a random permutation of [1..%d]\n", N);
-  return array;
-}
-
-// Verify that array is sorted
-//
-void verify_array(int *array, int N) {
-  for (int i = 0; i < N-1; i++)
-    if (array[i]>array[i+1]) {
-      printf("FAILED: array[%d]=%d, array[%d]=%d\n", 
-	     i, array[i], i+1, array[i+1]);
-      return;
-    }
-  printf("Result verified!\n");
+    if (i == j) return;
+    int tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
 }
 
 // Bubble sort for the base cases
 //
 void bubblesort(int *array, int low, int high) {
-  if (low >= high) 
-    return;
-  for (int i = low; i <= high; i++)
-    for (int j = i+1; j <= high; j++) 
-      if (array[i] > array[j])
-	swap(array, i, j);
+    if (low >= high)
+        return;
+    for (int i = low; i <= high; i++)
+        for (int j = i + 1; j <= high; j++)
+            if (array[i] > array[j])
+                swap(array, i, j);
 }
 
-// Pick an arbitrary element as pivot. Rearrange array 
+// Pick an arbitrary element as pivot. Rearrange array
 // elements into [smaller one, pivot, larger ones].
 // Return pivot's index.
 //
 int partition(int *array, int low, int high) {
-  int pivot = array[high]; 	// use highest element as pivot 
-  int middle = low;
-  for(int i = low; i < high; i++)
-    if(array[i] < pivot) {
-      swap(array, i, middle);
-      middle++;
-    }
-  swap(array, high, middle);
-  return middle;
+    int pivot = array[high];    // use highest element as pivot
+    int middle = low;
+    for (int i = low; i < high; i++)
+        if (array[i] < pivot) {
+            swap(array, i, middle);
+            middle++;
+        }
+    swap(array, high, middle);
+    return middle;
 }
- 
+
 // QuickSort an array range
-// 
+//
 void quicksort(int *array, int low, int high) {
-  if (high - low < MINSIZE) {
-    bubblesort(array, low, high);
-    return;
-  }
-  int middle = partition(array, low, high);
-  if (low < middle)
-    quicksort(array, low, middle-1);
-  if (middle < high)
-    quicksort(array, middle+1, high);
-}
- 
-// Main routine for testing quicksort
-// 
-int main(int argc, char **argv) {
-  int *array, N;
-  
-  // check command line first 
-  if (argc < 2) {
-    printf ("Usage: ./qsort <N>\n");
-    exit(0);
-  }
-  if ((N = atoi(argv[1])) < 2) {
-    printf ("<N> must be greater than 2\n");
-    exit(0);
-  }
-
-  array = init_array(N);
-
-  //  printf("Sorting started ...\n");
-  quicksort(array, 0, N-1);
-  //  printf("... completed.\n");
-
-  verify_array(array, N);
+    if (high - low < MINSIZE) {
+        bubblesort(array, low, high);
+        return;
+    }
+    int middle = partition(array, low, high);
+    if (low < middle)
+        quicksort(array, low, middle - 1);
+    if (middle < high)
+        quicksort(array, middle + 1, high);
 }
 
+int readData(char *fname, MPI_File *fh, MPI_Status *st, int *buf) {
+    int i = 0;
+    MPI_Offset size;
+    if (DEBUG) printf("Opening file %s\n", fname);
+    MPI_File_open(MPI_COMM_WORLD, fname, MPI_MODE_RDONLY, MPI_INFO_NULL, fh);
+    if (DEBUG) printf("Getting file size\n");
+    MPI_File_get_size(*fh, &size);
+    if (DEBUG) printf("File size is %d\n", size);
+    buf = (int *) malloc(size);
+    // set starting offset for the read operation
+    if (DEBUG) printf("Setting view\n");
+    MPI_File_set_view(*fh, 0, MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
+    // read two integers from the file
+    if (DEBUG) printf("Reading file\n");
+    MPI_File_read(*fh, buf, size / 4, MPI_INT, st);
+
+    for (i = 0; i < size / 4; i++) {
+        printf("%d %d\n", i, buf[i]);
+    }
+    if (DEBUG) printf("Size of int %d\n", sizeof(int));
+    return size / 4;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Datafile name is required\n");
+        return 0;
+    }
+
+
+    int size, rank, *buf, message, tag = 201;
+    MPI_File fh;
+    MPI_Status st;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int nextRank = (rank + 1) % size;
+    int prevRank = (rank + size - 1) % size;
+    if (rank == 0) {
+        int bufSize = 0;
+        int toSort = 10 * size;
+        /*
+         * 1. Read all data
+         * 2. sort first 10*size elements, and selects elements at positions 10, 20, 10(size-1)
+         *      as pivots
+         * 3. Partition data into P buckets
+         */
+        // quicksort(array, 0, N-1);
+        message = size;
+//        MPI_Send(&message,1,MPI_INT,nextRank,tag,MPI_COMM_WORLD);
+        printf("Process %d reading %s\n", rank, argv[1]);
+        bufSize = readData(argv[1], &fh, &st, buf);
+        quicksort(buf, 0, toSort - 1);
+//        printf("Process %d sent to %d value: %d\n", rank, nextRank, message);
+    }
+
+//    while (1)
+//    {
+//        MPI_Recv(&message, 1, MPI_INT, prevRank, tag, MPI_COMM_WORLD,
+//                 MPI_STATUS_IGNORE);
+//
+//        if (message != 0) {
+//            --message;
+//            printf("Process %d decremented value: %d\n", rank, message);
+//        }
+//
+//        MPI_Send(&message, 1, MPI_INT, nextRank, tag, MPI_COMM_WORLD);
+//
+//        if (0 == message) {
+//            printf("Process %d exiting\n", rank);
+//            break;
+//        }
+//    }
+
+    MPI_Finalize();
+    return 0;
+}
